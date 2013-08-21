59e3b37a8c9374112544f09be08b3d265d38db87
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-08-21 14:24:04 +0900

    Use all leds.

--- samples/Haskell/hs_src/Main.hs
+++ samples/Haskell/hs_src/Main.hs
@@ -1,4 +1,5 @@
 import Control.Monad
+import Data.Word
 
 import Led
 import Delay
@@ -7,14 +8,25 @@ main :: IO ()
 main = do
   initLeds
   realmain
-  
+
+ledList :: [Word32]
+ledList = [led1, led2, led3, led4]
+
 realmain :: IO ()
 realmain = forever $ do
-  ledsOn []
+  ledsOn $ take 1 ledList
   delay 1000000
-  ledsOn [led1]
+  ledsOn $ take 2 ledList
   delay 1000000
-  ledsOn [led1, led2]
+  ledsOn $ take 3 ledList
   delay 1000000
-  ledsOn [led2]
+  ledsOn $ take 4 ledList
+  delay 1000000
+  ledsOn $ drop 1 ledList
+  delay 1000000
+  ledsOn $ drop 2 ledList
+  delay 1000000
+  ledsOn $ drop 3 ledList
+  delay 1000000
+  ledsOn $ drop 4 ledList
   delay 1000000
